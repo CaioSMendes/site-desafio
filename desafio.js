ee899a50f5ce79aae75780a5f1f9ecbda4995fb0
Caio Mendes <caiomendes@cjr.org.br>
2018-05-28 23:36:11 -0300

    site desafio

--- desafio.js
+++ desafio.js
@@ -4,38 +4,36 @@ var aux=0;
 var total=0;
 var val=0;
 
-function tabela(){
-	
-	val = valores;
-	console.log("val tabela->",val)
-}
-
 function calcula(){
 	var gastos = document.getElementById("teste")
 	var valores = document.calcform.valores.value;
 	var sinal = document.getElementById("sinal").value;
-	var suporte = document.getElementById("desc-renda2").value;
-
-		
+	var div = document.createTextNode(gastos)
+	var div1 = document.createElement("gastos");
 	
 	salario=gastos;
-	console.log(gastos);
+	console.log("string ->",div);
+	console.log("gastos ->",gastos);
 	console.log(valores);
 	console.log(sinal);
-
-	
 
 		if(sinal == "adicionar"){
 			resultado = resultado + parseFloat(valores);
 			gastos.innerHTML  = resultado.toString() 
-			// console.log("resultado->",resultado);
+			div1.innerHTML = gastos;
+			document.getElementById("renda-desc").appendChild(div1);
+     	    document.getElementById("valor-renda").innerHTML=valores;//aqui
+ 
+
 		}else{
 			aux = aux + parseFloat(valores);
 			console.log("aux->",aux);
 			var gastos = document.getElementById("teste2")
 			gastos.innerHTML = aux.toString()
+			document.getElementById("desp-desc").innerHTML=gastos.toString();
+			document.getElementById("valor-desp").innerHTML=valores;
+		}
 
-        }
         total = parseFloat(resultado) - parseFloat(aux);
         console.log("total->",total);
         var gastos = document.getElementById("teste0")
@@ -45,5 +43,3 @@ function calcula(){
 			}
 
 }
-
-
